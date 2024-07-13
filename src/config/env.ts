d82a620bc279021895659ea513d3e8a341a32ada
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-07-12 23:39:22 -0300

    feat: :sparkles: add env config

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+const KEYS = {
+  YOUTUBE: 'AIzaSyDlxAohx4kitZLN3MdHdPzZID4lujINbWY',
+  TAG_STORAGE_TOKEN: '@tag',
+};
+
+const URLS = {
+  API_BASE_URL: 'https://www.googleapis.com/youtube/v3',
+  SEARCH_VIDEOS: '/search',
+  EMBED_VIDEO: 'https://www.youtube.com/embed',
+};
+
+export const ENV = {
+  KEYS,
+  URLS,
+};
